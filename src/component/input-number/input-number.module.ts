2507ff43877d3249c1adac6e090b6f5005990e11
LaamGinghong <lanjinghang@banggood.com>
2019-01-02 12:27:35 +0800

    修改bug

--- src/component/input-number/input-number.module.ts
+++ src/component/input-number/input-number.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {InputNumberComponent} from './input-number.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 
 @NgModule({
   declarations: [InputNumberComponent],
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgZorroAntdModule
   ],
   exports: [InputNumberComponent]
 })
