fe20ac65988962f8e6e25f49b3b021e13ee673db
LaamGinghong <lanjinghang@banggood.com>
2019-01-08 14:28:08 +0800

    v0.0.27

---
+++ projects/bang-storage-ui/src/lib/zoom/storage-zoom.component.ts
@@ -0,0 +1,41 @@
+import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'storage-zoom',
+  template: `
+    <nz-layout>
+      <nz-content>
+        <img [src]="path" alt="放大图片" (click)="zoomOut.emit()">
+      </nz-content>
+    </nz-layout>
+  `,
+  styles: [`
+    nz-layout {
+      width: 100%;
+      height: 100%;
+      position: fixed;
+      background: rgba(0, 0, 0, .45) !important;
+      top: 0;
+      left: 0;
+    }
+
+    nz-content {
+      max-width: 60%;
+      margin: 100px auto 0;
+      cursor: zoom-out;
+    }
+
+    nz-content > img {
+      width: 100%;
+    }
+  `],
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false
+})
+export class StorageZoomComponent {
+  @Input() path: string;
+  @Output() zoomOut = new EventEmitter<boolean>();
+
+  constructor() {
+  }
+}
