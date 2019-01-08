d3bfd86e38bbe7865d259cb5f6acfad526e646c8
LaamGinghong <lanjinghang@banggood.com>
2019-01-08 14:28:39 +0800

    添加到projects

--- projects/bang-storage-ui/src/lib/bang-storage-ui.module.ts
+++ projects/bang-storage-ui/src/lib/bang-storage-ui.module.ts
@@ -2,16 +2,19 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {StorageButtonModule} from './button';
 import {StorageInputNumberModule} from './input-number';
 import {StorageModalModule} from './modal';
+import {StorageZoomModule} from './zoom';
 
 export * from './button';
 export * from './input-number';
 export * from './modal';
+export * from './zoom';
 
 @NgModule({
   exports: [
     StorageButtonModule,
     StorageInputNumberModule,
-    StorageModalModule
+    StorageModalModule,
+    StorageZoomModule
   ]
 })
 export class BangStorageUiModule {
---
+++ projects/bang-storage-ui/src/lib/zoom/public_api.ts
@@ -0,0 +1,4 @@
+export * from './storage-zoom.component';
+export * from './storage-zoom.directive';
+export * from './storage-zoom.module';
+
