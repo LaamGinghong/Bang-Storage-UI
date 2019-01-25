569ebd4c5f9c60236f24264f6e38e569cf4ff814
LaamGinghong <lanjinghang@banggood.com>
2019-01-25 14:12:31 +0800

    zoom的蒙层添加z-index使之成为绝对顶层

--- src/component/zoom/storage-zoom.component.ts
+++ src/component/zoom/storage-zoom.component.ts
@@ -14,6 +14,7 @@ import {Component, EventEmitter, Input, Output, ViewEncapsulation} from '@angula
       width: 100%;
       height: 100%;
       position: fixed;
+      z-index: 999999999999999;
       background: rgba(0, 0, 0, .45) !important;
       top: 0;
       left: 0;
