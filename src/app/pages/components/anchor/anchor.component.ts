d163ba3ffeea144a762036f6f56f466db89b8d74
LaamGinghong <lanjinghang@banggood.com>
2019-02-14 16:29:25 +0800

    完成anchor锚点文档

--- src/app/pages/components/anchor/anchor.component.ts
+++ src/app/pages/components/anchor/anchor.component.ts
@@ -7,7 +7,31 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./anchor.component.less']
 })
 export class AnchorComponent implements OnInit {
+  codeBox = {
+    first: {
+      show: false,
+      source: `
+      import {Component} from '@angular/core';
 
+      @Component({
+        selector: 'storage-anchor-demo',
+        template: \`
+                  <storage-anchor>
+                    <storage-link storageHref="#use" storageTitle="何时使用"></storage-link>
+                    <storage-link storageHref="#example" storageTitle="代码演示"></storage-link>
+                    <storage-link storageHref="#api" storageTitle="API"></storage-link>
+                  </storage-anchor>
+                  \`
+      })
+      export class StorageAnchorDemoComponent {
+      }
+      `
+    }
+  };
+  tableData = [
+    {name: '[storageHref]', description: '锚点链接', type: 'string', default: '-'},
+    {name: '[storageTitle]', description: '文字内容', type: 'string', default: '-'}
+  ];
 
   constructor() {
   }
--- src/app/pages/components/anchor/anchor.module.ts
+++ src/app/pages/components/anchor/anchor.module.ts
@@ -2,12 +2,14 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AnchorComponent} from './anchor.component';
 import {AnchorRoutingModule} from './anchor-routing.module';
+import {ShareModule} from '../../../share/share.module';
 
 @NgModule({
   declarations: [AnchorComponent],
   imports: [
     CommonModule,
-    AnchorRoutingModule
+    AnchorRoutingModule,
+    ShareModule
   ]
 })
 export class AnchorModule {
