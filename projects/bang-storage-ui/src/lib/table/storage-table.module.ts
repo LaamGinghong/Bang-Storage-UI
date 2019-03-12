21fddd0e0b6fc69d94dcb6816b19ac90c63c5321
LaamGinghong <lanjinghang@banggood.com>
2019-03-12 11:28:16 +0800

    添加表格tbody td组件

--- projects/bang-storage-ui/src/lib/table/storage-table.module.ts
+++ projects/bang-storage-ui/src/lib/table/storage-table.module.ts
@@ -4,11 +4,27 @@ import {StorageTableComponent} from './storage-table.component';
 import {StorageTheadComponent} from './thead/storage-thead.component';
 import {StorageTrComponent} from './tr/storage-tr.component';
 import {StorageThComponent} from './th/storage-th.component';
+import {StorageTbodyComponent} from './tbody/storage-tbody.component';
+import {StorageTdComponent} from './td/storage-td.component';
 
 @NgModule({
   imports: [CommonModule],
-  exports: [StorageTableComponent, StorageTheadComponent, StorageTrComponent, StorageThComponent],
-  declarations: [StorageTableComponent, StorageTheadComponent, StorageTrComponent, StorageThComponent]
+  exports: [
+    StorageTableComponent,
+    StorageTheadComponent,
+    StorageTrComponent,
+    StorageThComponent,
+    StorageTbodyComponent,
+    StorageTdComponent
+  ],
+  declarations: [
+    StorageTableComponent,
+    StorageTheadComponent,
+    StorageTrComponent,
+    StorageThComponent,
+    StorageTbodyComponent,
+    StorageTdComponent
+  ]
 })
 export class StorageTableModule {
 }
---
+++ projects/bang-storage-ui/src/lib/table/tbody/storage-tbody.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'storage-tbody',
+  templateUrl: './storage-tbody.component.html',
+  styleUrls: ['./storage-tbody.component.less']
+})
+export class StorageTbodyComponent {
+}
